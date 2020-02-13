5c47ff7b0a773a27ca043916c04b76b25f6473d2
Javier Hevia <javierhc22@gmail.com>
2020-02-13 16:57:48 -0600

    Arreglo de errores

--- app.js
+++ app.js
@@ -1,87 +1,72 @@
-//////////////////////
-// npm run dev //
-/////////////////////
-
-var request = require("request");
-
-var http = require('http');
-
-var express = require('express');
-var app = express();
+// npm run dev
+var express = require('express')
+var app = express()
 
 var bodyParser = require('body-parser')
-var path = require('path'); // para llamar paginas ya creadas.
+var path = require('path') // para llamar paginas ya creadas.
 
-app.use(express.static(__dirname + '/public/')); // jala mi index por default
+app.use(express.static(__dirname + '/public/')) // jala mi index por default
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ limit: '10mb' }))
 
 app.get('/', function (req, res) { // ruta predefinida
-    res.send('Hola Mundo!');
-});
+  res.send('Hola Mundo!')
+})
 
 app.post('/Pedido', function (req, res) {
+  console.log('Bienvenido al servicio de SERVICIO DE CROWDSOURCING DE COMIDA A DOMICILIO')
+  console.log('.')
+  console.log('..')
+  console.log('...')
+  // recibo los datos del form y los guardo en variables para mandarlos como parametros por la url
+  var Restaurante = req.body.r2
+  var menu = req.body.m2
+  var bebida = req.body.b2
 
-    console.log("Bienvenido al servicio de SERVICIO DE CROWDSOURCING DE COMIDA A DOMICILIO");
-    console.log(".");
-    console.log("..");
-    console.log("...");
-    //recibo los datos del form y los guardo en variables para mandarlos como parametros por la url
-    var Restaurante = req.body.r2;
-    var menu = req.body.m2;
-    var bebida = req.body.b2;
-
-    console.log("Pedido realizado con éxito");
-    console.log("...");
-    console.log("..");
-    console.log(".");
-    //mando a llamar a mi get y le mando los parametros
-    res.redirect('/recepcionOrdenes?restaurante=' + Restaurante + '&menu=' + menu + '&bebida=' + bebida);
-
-});
+  console.log('Pedido realizado con éxito')
+  console.log('...')
+  console.log('..')
+  console.log('.')
+  // mando a llamar a mi get y le mando los parametros
+  res.redirect('/recepcionOrdenes?restaurante=' + Restaurante + '&menu=' + menu + '&bebida=' + bebida)
+})
 
 app.get('/recepcionOrdenes', function (req, res) {
-
-    //mando a llamar al html de recepciones de ordenes por medio de un get
-    res.sendFile(path.join(__dirname + '/public/RecepcionOrdenes.html'));
-
-});
-
+  // mando a llamar al html de recepciones de ordenes por medio de un get
+  res.sendFile(path.join(__dirname + '/public/RecepcionOrdenes.html'))
+})
 
 app.post('/EnviarARepartido', function (req, res) {
-
-    //guardo el dato de mi form y se lo mando como parametro al get de enviar repartidor
-    var dato = req.body.datosenviar;
-    console.log(".");
-    console.log("..");
-    console.log("...");
-    console.log("Pedido enviado al repartido con éxito");
-    console.log("...");
-    console.log("..");
-    console.log(".");
-    res.redirect('/EnviarARepartido2?datos=' + dato);
-});
+  //  guardo el dato de mi form y se lo mando como parametro al get de enviar repartidor
+  var dato = req.body.datosenviar
+  console.log('.')
+  console.log('..')
+  console.log('...')
+  console.log('Pedido enviado al repartido con éxito')
+  console.log('...')
+  console.log('..')
+  console.log('.')
+  res.redirect('/EnviarARepartido2?datos=' + dato)
+})
 
 app.get('/EnviarARepartido2', function (req, res) {
-    //mando a llamar por medio de un get a mi pagina de servicio de entrega
-    res.sendFile(path.join(__dirname + '/public/ServicioEntrega.html'));
-});
-
+  // mando a llamar por medio de un get a mi pagina de servicio de entrega
+  res.sendFile(path.join(__dirname + '/public/ServicioEntrega.html'))
+})
 
 app.post('/PedidoEnviado', function (req, res) {
-    //hago el envio del pedido y regreso a la pagina index
-    console.log(".");
-    console.log("..");
-    console.log("...");
-    console.log("Pedido enviado a su casa");
-    console.log("Gracias por utilizar este servicio");
-    console.log("...");
-    console.log("..");
-    console.log(".");
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
-
+  // hago el envio del pedido y regreso a la pagina index
+  console.log('.')
+  console.log('..')
+  console.log('...')
+  console.log('Pedido enviado a su casa')
+  console.log('Gracias por utilizar este servicio')
+  console.log('...')
+  console.log('..')
+  console.log('.')
+  res.sendFile(path.join(__dirname + '/public/index.html'))
+})
 
 app.listen(3000, function () {
-    console.log('Trabajando................... http://localhost:3000/');
-});
+  console.log('Trabajando................... http://localhost:3000/')
+})
